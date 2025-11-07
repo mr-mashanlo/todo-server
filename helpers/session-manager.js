952da382fbe371006c025960090088f828870266
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-11-07 18:07:46 +0500

    change cookie secure value

--- helpers/session-manager.js
+++ helpers/session-manager.js
@@ -5,8 +5,8 @@ import { ErrorManager } from './error-manager.js';
 export class SessionManager {
 
   issue = ( res, payload ) => {
-    const token = jwt.sign( payload, process.env.SECRET_KEY || '', { expiresIn: '2h' } );
-    res.cookie( 'token', token, { maxAge: 7_200_000, httpOnly: true, sameSite: 'none', secure: false } );
+    const token = jwt.sign( payload, process.env.SECRET_KEY, { expiresIn: '2h' } );
+    res.cookie( 'token', token, { maxAge: 7_200_000, httpOnly: true, sameSite: 'none', secure: true } );
     return { token, expired: Date.now() + 7_200_000 };
   };
 
@@ -16,7 +16,7 @@ export class SessionManager {
 
   verify = ( token ) => {
     try {
-      return jwt.verify( token, process.env.SECRET_KEY || '' );
+      return jwt.verify( token, process.env.SECRET_KEY );
     } catch ( error ) {
       throw new ErrorManager( { status: 400, issues: [ { name: 'token', message: error.message } ] } );
     }
