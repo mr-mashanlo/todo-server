795950dfa8086bc8e8849a13cacc8e5345e635d2
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-11-07 17:47:37 +0500

    change cookie sameSite value

--- helpers/session-manager.js
+++ helpers/session-manager.js
@@ -6,7 +6,7 @@ export class SessionManager {
 
   issue = ( res, payload ) => {
     const token = jwt.sign( payload, process.env.SECRET_KEY || '', { expiresIn: '2h' } );
-    res.cookie( 'token', token, { maxAge: 7_200_000, httpOnly: true, sameSite: 'lax', secure: false } );
+    res.cookie( 'token', token, { maxAge: 7_200_000, httpOnly: true, sameSite: 'none', secure: false } );
     return { token, expired: Date.now() + 7_200_000 };
   };
 
